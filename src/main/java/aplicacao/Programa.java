e0903ce6565cf000245021b2cffed4baded80c6f
Fabio Prudencio <fabio.oliveira.prudencio@gmail.com>
2021-12-25 23:48:30 -0300

    Removendo registro do banco de dados

--- src/main/java/aplicacao/Programa.java
+++ src/main/java/aplicacao/Programa.java
@@ -8,22 +8,20 @@ import dominio.Pessoa;
 
 public class Programa {
 
-	public static void main(String[] args) {		
+	public static void main(String[] args) {
 		//Pessoa p1 = new Pessoa(null, "Carlos da Silva", "carlos@gmail.com");
-		//Pessoa p2 = new Pessoa(null, "Joaquim Torres", "joaquim@gmail.com");
+		Pessoa p2 = new Pessoa(null, "Joaquim Torres", "joaquim@gmail.com");
 		//Pessoa p3 = new Pessoa(null, "Ana Maria", "ana@gmail.com");
 		
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
 		EntityManager em = emf.createEntityManager();
 		
-		em.getTransaction().begin();
-		//em.persist(p1); //Inserir
-		
+		em.getTransaction().begin(); //iniciar transação		
+		em.persist(p2); //Inserir		
 		Pessoa p = em.find(Pessoa.class, 2); //Busca uma pessoa que o id seja igual a 2
-		
-		System.out.println(p);
-
-		em.getTransaction().commit();
+		System.out.println(p); 		
+		em.remove(p); //remove
+		em.getTransaction().commit(); //encerrar transação
 		em.close();
 		emf.close();		
 		
